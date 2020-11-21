5352de94814422a307e369ccdadf802409bb6aa4
Anton Drozd <anton.drozd.95@gmail.com>
2020-11-21 20:43:40 +0200

    Add SignUp component mock-up

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,80 @@
+import { Component } from 'react'
+
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+
+import './sign-up.styles.scss'
+
+class SignUp extends Component {
+  state = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const { password, confirmPassword } = this.state
+
+    if (password !== confirmPassword) {
+      alert("Passwords don't match")
+      return
+    }
+  }
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state
+
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have a account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            label="Display Name"
+            value={displayName}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            label="Email"
+            value={email}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            label="Password"
+            value={password}
+            handleChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            label="Confirm Password"
+            value={confirmPassword}
+            handleChange={this.handleChange}
+            required
+          />
+          <Button type="submit">Sign Up</Button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignUp
