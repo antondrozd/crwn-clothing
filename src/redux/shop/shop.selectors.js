e9505761f1f7b9b54906b85a805e6833c096b950
Anton Drozd <anton.drozd.95@gmail.com>
2021-01-17 04:05:14 +0200

    Set up nested shop page routes

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,3 +1,9 @@
 export const selectShop = (state) => state.shop
 
 export const selectShopCollections = (state) => selectShop(state).collections
+
+export const selectShopCollection = (collectionRouteName) => (state) => {
+  return selectShopCollections(state).find(
+    (collection) => collection.routeName === collectionRouteName
+  )
+}
